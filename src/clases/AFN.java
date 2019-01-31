92a10cdf1641745840a447702ec1a589e2559ca5
tapia <tapia641@gmail.com>
2019-01-31 10:57:04 -0600

    First commit

---
+++ src/clases/AFN.java
@@ -0,0 +1,11 @@
+package clases;
+
+import java.util.HashSet;
+
+public class AFN {
+    static int Id = 0;
+    static int Epsilon = 300;
+    public HashSet<Integer> Estados;
+    public HashSet<Integer> s;
+
+}
