3cfba0c4e339dd986b42af315443140de516cf73
tapia <tapia641@gmail.com>
2019-05-16 13:45:07 -0500

    AS: Calculadora evalua los numeros

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -3,6 +3,8 @@ import Clases.AFN;
 import Clases.AnalizadorLexico;
 import Clases.AnalizadorSintactico.AritmeticaBasica;
 import Clases.AnalizadorSintactico.Calculadora;
+import Clases.AnalizadorSintactico.ExpresionesRegulares;
+import Clases.Tokens;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -29,15 +31,52 @@ public class Pruebas {
         f15 = new AFN();
         f16 = new AFN();
 
+        //GRAMATICA DE EXPRESIONES REGULARES
 /*
+        AFD afd1 = new AFD();
+
+        f1.CrearBasico('&');  f1.setToken(Tokens.ER_CONCA);
+        f2.CrearBasico("|"); f2.setToken(Tokens.ER_OR);
+        f3.CrearBasico('+');  f3.setToken(Tokens.ER_CERRADURA_POSTIVIA);
+        f4.CrearBasico('*');  f4.setToken(Tokens.ER_CERRADURA_ESTRELLA);
+        f5.CrearBasico('A','Z'); f5.setToken(Tokens.ER_SIMBOLO);
+        f6.CrearBasico('?'); f6.setToken(Tokens.ER_THOMPSON);
+        f7.CrearBasico('('); f7.setToken(Tokens.ER_PAR_I);
+        f8.CrearBasico(')'); f8.setToken(Tokens.ER_PAR_D);
+
+        conjuntoAFN.add(f1);
+        conjuntoAFN.add(f2);
+        conjuntoAFN.add(f3);
+        conjuntoAFN.add(f4);
+        conjuntoAFN.add(f5);
+        conjuntoAFN.add(f6);
+        conjuntoAFN.add(f7);
+        conjuntoAFN.add(f8);
+
+
         try {
-            TablaLL1 TLL1 = new TablaLL1("Gramatica1.txt");
-            System.out.println(TLL1.First("E"));
+            afd1.convertirAFD(conjuntoAFN,"ExpresionesRegulares");
         } catch (IOException e) {
             e.printStackTrace();
         }
-*/
+        */
 
+/*
+        AFD afd = new AFD();
+        ExpresionesRegulares ER = new ExpresionesRegulares();
+        AnalizadorLexico AnalizarLexicamente = new AnalizadorLexico();
+        try {
+            //IMPORTAMOS HASMAP DE GRAMATICA CALCULADORA PREVIAMENTE REALIZADA
+            afd.LeerObject("ExpresionesRegulares");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String cadena = "(A|B)*&C+";
+        AnalizarLexicamente.Lexico(cadena, afd.getMatriz());
+        ER.AnalizarSintacticamente(AnalizarLexicamente.getPila(),AnalizarLexicamente.getTablaLexema());
+*/
 /*
     //Para convertir una AFD a un .out
         AFD afd = new AFD();
@@ -63,8 +102,9 @@ public class Pruebas {
         }
         String cadena = "4.5*9+SIN(17-14/8)*6";
         AnalizarLexicamente.Lexico(cadena, afd.getMatriz());
-        C.AnalizarSintacticamente(AnalizarLexicamente.getPila());
-/*
+        C.AnalizarSintacticamente(AnalizarLexicamente.getTablaLexema());
+
+        /*
         AFD afd1 = new AFD();
         AritmeticaBasica AB = new AritmeticaBasica();
         AnalizadorLexico AnalizarLexicamente = new AnalizadorLexico();
@@ -86,5 +126,6 @@ public class Pruebas {
         AnalizarLexicamente.Lexico(cadena, afd1.getMatriz());
         AB.AnalizarSintacticamente(AnalizarLexicamente.getPila());
         */
+
     }
 }
